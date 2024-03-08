bf84bea2f959f142cf5c800ca8f922620030c6f6
mattomar <moddathero@gmail.com>
2024-03-08 08:10:34 +0700

    added the menu and styled it

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ document.addEventListener('DOMContentLoaded',() =>{
     const homeTab = document.getElementById('home-tab')
     homeTab.addEventListener('click', createHomePage)
 })
+
+
+import { createMenuPage } from "./menu";
+
+document.addEventListener('DOMContentLoaded',() =>{
+    const menuTab = document.getElementById('menu-tab')
+    menuTab.addEventListener('click', createMenuPage)
+})
---
+++ src/menu.js
@@ -0,0 +1,62 @@
+export function setHTMLContent(html) {
+    const contentDiv = document.getElementById("content")
+    contentDiv.innerHTML = html
+}
+
+
+export function createMenuPage() {
+    const MenuContent =
+    `
+    <div class="card-container">
+    <div class="card">
+      <div class="card-flex-container">
+        <img src="/assets/imgs/pizza-hero.png" alt="pizza photo in the menu" class="menu-pic">
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, consectetur.</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-flex-container">
+        <img src="/assets/imgs/pizza-hero.png" alt="pizza photo in the menu" class="menu-pic">
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, consectetur.</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-flex-container">
+        <img src="/assets/imgs/pizza-hero.png" alt="pizza photo in the menu" class="menu-pic">
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, consectetur.</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-flex-container">
+        <img src="/assets/imgs/pizza-hero.png" alt="pizza photo in the menu" class="menu-pic">
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, consectetur.</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-flex-container">
+        <img src="/assets/imgs/pizza-hero.png" alt="pizza photo in the menu" class="menu-pic">
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, consectetur.</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-flex-container">
+        <img src="/assets/imgs/pizza-hero.png" alt="pizza photo in the menu" class="menu-pic">
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, consectetur.</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-flex-container">
+        <img src="/assets/imgs/pizza-hero.png" alt="pizza photo in the menu" class="menu-pic">
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, consectetur.</p>
+      </div>
+    </div>
+  </div>`
+setHTMLContent(MenuContent)
+}
